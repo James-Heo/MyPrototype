03d2d8fc09164ccab110f13607a93db8caf5de0f
Mr. Satan <0421jamesheo@gmail.com>
2020-02-20 00:33:04 -0500

    Pick Up now destroyed when collided
    When the laser collides with the pick up the pick up is now destroyed

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -7,12 +7,20 @@ public class Rotation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Laser")
+        {
+            Destroy(gameObject);
+        }
     }
 }
